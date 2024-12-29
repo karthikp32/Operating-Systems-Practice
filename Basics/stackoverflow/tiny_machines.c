f369ef5e6d126781c49f15b3a2d301718594f1ae
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2024-12-29 22:22:49 +0000

    calculating the number of bytes of data allocated in stack memory for the parameters in build_tiny_machine()

--- Basics/stackoverflow/tiny_machines.c
+++ Basics/stackoverflow/tiny_machines.c
@@ -1,12 +1,16 @@
 #include <float.h>
 #include <stdio.h>
 
-void build_tiny_machine(long double size, int fx_call) {
+void build_tiny_machine(long double size, int fx_call, int total_bytes_in_stack) {
     fx_call++;
+    total_bytes_in_stack += sizeof(size) + sizeof(fx_call) + sizeof(total_bytes_in_stack);
     printf("In build_tiny_machine call %d current tiny machine size is %Lg meters\n", fx_call, size);
-    build_tiny_machine(size/2, fx_call);
+    printf("Size of parameters: %d bytes\n", total_bytes_in_stack);    
+    build_tiny_machine(size/2, fx_call, total_bytes_in_stack);
+    printf("built a tiny machine!");
 }
 
 int main(int argc, char *argv[]) {
-    build_tiny_machine(LDBL_MAX, 0);
+
+    build_tiny_machine(LDBL_MAX, 0, 0);
 }
