1d037953998eb2dda3a4e7b37b6dc9f423c1fba4
Karthik Pillalamarri <karthik.pillalamarri.kp@gmail.com>
2025-01-12 23:42:43 +0000

    able to get memory address of first variable in current stack frame

--- Basics/stackoverflow/tiny_machines.c
+++ Basics/stackoverflow/tiny_machines.c
@@ -1,9 +1,12 @@
 #include <float.h>
 #include <stdio.h>
+#include <stdint.h>
+
 
 void build_tiny_machine(long double size, int fx_call, int total_bytes_in_stack) {
+    printf("Memory address of first variable in current stack frame is (%p)", &total_bytes_in_stack);
     fx_call++;
-    total_bytes_in_stack += sizeof(size) + sizeof(fx_call) + sizeof(total_bytes_in_stack);
+    // total_bytes_in_stack += sizeof(size) + sizeof(fx_call) + sizeof(total_bytes_in_stack);
     printf("In build_tiny_machine call %d current tiny machine size is %Lg meters\n", fx_call, size);
     printf("Size of parameters: %d bytes\n", total_bytes_in_stack);    
     build_tiny_machine(size/2, fx_call, total_bytes_in_stack);
@@ -11,6 +14,5 @@ void build_tiny_machine(long double size, int fx_call, int total_bytes_in_stack)
 }
 
 int main(int argc, char *argv[]) {
-
     build_tiny_machine(LDBL_MAX, 0, 0);
 }
